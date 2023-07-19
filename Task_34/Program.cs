485fb4c8a39593f4f9befc7cb40757adf71dfd48
Michael_Fesenko <@gmail.com>
2023-07-19 20:12:26 +0300

    добавил Task_34

---
+++ Task_34/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] GenerateRandomArray(int size, int from, int to)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(from, to);
+    }
+    return array;
+}
+
+int[] mas = GenerateRandomArray(10, 100, 1000);
+Console.WriteLine($"[{string.Join(", ", mas)}]");
+Console.WriteLine($"Количество четных чисел в массиве равно: {CountEven(mas)}");
